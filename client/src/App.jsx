8332890a16230b132d0e7bac366e7b83cdc7b8eb
Danish <dan71ish@gmail.com>
2024-03-10 23:03:47 +0530

    added 404 route and also make changes in title and favicon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import { lookInSession } from "./common/session.jsx";
 import Editor from "./pages/editor.pages.jsx";
 import HomePage from "./pages/home.page.jsx";
 import SearchPage from "./pages/search.page.jsx";
+import PageNotFound from "./pages/404.page.jsx";
 
 export const UserContext = createContext({});
 const App = () => {
@@ -27,6 +28,7 @@ const App = () => {
             <Route path="signin" element={<UserAuthForm type="sign-in" />} />
             <Route path="signup" element={<UserAuthForm type="sign-up" />} />
             <Route path="search/:query" element={<SearchPage />} />
+            <Route path="*" element={<PageNotFound />} />
           </Route>
           <Route path="/editor" element={<Editor />} />
         </Routes>
