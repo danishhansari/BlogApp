6fdef74c2f680a78a5ad02851d4629e9d2a42526
Danish <dan71ish@gmail.com>
2024-03-10 22:36:11 +0530

    search route to work on mobile pages

--- client/src/pages/search.page.jsx
+++ client/src/pages/search.page.jsx
@@ -8,10 +8,23 @@ import LoadMoreDataBtn from "../components/load-more.component";
 import { useEffect, useState } from "react";
 import { filterPagination } from "../common/filter-pagination-data";
 import axios from "axios";
+import UserCard from "../components/usercard.component";
 
 const SearchPage = () => {
   const { query } = useParams();
   const [blogs, setBlogs] = useState(null);
+  const [users, setUsers] = useState(null);
+
+  const fetchUsers = () => {
+    axios
+      .post(`${import.meta.env.VITE_SERVER_LOCATION}/search-users`, {
+        query,
+      })
+      .then(({ data: { user } }) => {
+        console.log(user);
+        setUsers(user);
+      });
+  };
 
   const searchBlogs = ({ page = 1, createNewArr = false }) => {
     axios
@@ -38,12 +51,36 @@ const SearchPage = () => {
 
   useEffect(() => {
     resetState();
+    fetchUsers();
     searchBlogs({ page: 1, createNewArr: true });
   }, [query]);
 
   const resetState = () => {
-    console.log("I got a call");
     setBlogs(null);
+    setUsers(null);
+  };
+
+  const UserCardWrapper = () => {
+    return (
+      <>
+        {users === null ? (
+          <Loader />
+        ) : users.length ? (
+          users.map((user, i) => {
+            return (
+              <AnimationWrapper
+                key={i}
+                transition={{ duration: 1, delay: i * 0.08 }}
+              >
+                <UserCard user={user} />
+              </AnimationWrapper>
+            );
+          })
+        ) : (
+          <NoDataMessage message={"No user found"} />
+        )}
+      </>
+    );
   };
 
   return (
@@ -53,30 +90,32 @@ const SearchPage = () => {
           <InPageNavigation
             routes={[`Search Results from "${query}"`, "Account Matched"]}
             defaultHidden={["Account Matched"]}
-          />
+          >
+            <>
+              {blogs === null ? (
+                <Loader />
+              ) : blogs.results.length ? (
+                blogs.results.map((blog, i) => {
+                  return (
+                    <AnimationWrapper
+                      key={i}
+                      transition={{ duration: 1, delay: i * 0.1 }}
+                    >
+                      <BlogPostCard
+                        content={blog}
+                        author={blog.author.personal_info}
+                      />
+                    </AnimationWrapper>
+                  );
+                })
+              ) : (
+                <NoDataMessage message="No blogs published" />
+              )}
+              <LoadMoreDataBtn state={blogs} fetchDataFn={searchBlogs} />
+            </>
 
-          <>
-            {blogs === null ? (
-              <Loader />
-            ) : blogs.results.length ? (
-              blogs.results.map((blog, i) => {
-                return (
-                  <AnimationWrapper
-                    key={i}
-                    transition={{ duration: 1, delay: i * 0.1 }}
-                  >
-                    <BlogPostCard
-                      content={blog}
-                      author={blog.author.personal_info}
-                    />
-                  </AnimationWrapper>
-                );
-              })
-            ) : (
-              <NoDataMessage message="No blogs published" />
-            )}
-            <LoadMoreDataBtn state={blogs} fetchDataFn={searchBlogs} />
-          </>
+            <UserCardWrapper />
+          </InPageNavigation>
         </div>
       </section>
     </>
