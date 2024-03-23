64f7f135eb6838eee095d4d6a0661242790d6229
Danish <dan71ish@gmail.com>
2024-03-23 12:37:12 +0530

    Added the delete fn to comments

--- client/src/components/comment-card.component.jsx
+++ client/src/components/comment-card.component.jsx
@@ -4,11 +4,12 @@ import { UserContext } from "../App";
 import { toast } from "react-hot-toast";
 import CommentField from "./comment-field.component";
 import { BlogContext } from "../pages/blog.page";
+import axios from "axios";
 
 const CommentsCard = ({ index, leftVal, commentsData }) => {
   let {
     commented_by: {
-      personal_info: { profile_img, fullname, username },
+      personal_info: { profile_img, fullname, username: commented_by_user },
     },
     commentedAt,
     comment,
@@ -21,16 +22,33 @@ const CommentsCard = ({ index, leftVal, commentsData }) => {
     blog: {
       comments,
       comments: { results: commentsArr },
+      author: {
+        personal_info: { username: blog_author },
+      },
     },
     setBlog,
+    setTotalParentCommentsLoaded,
   } = useContext(BlogContext);
 
   const [isReplying, setReplying] = useState(false);
   const {
-    userAuth: { access_token },
+    userAuth: { access_token, username },
   } = useContext(UserContext);
 
-  const removeCommentsCard = (startingPoint) => {
+  const getParentIndex = () => {
+    let startingPoint = index - 1;
+    try {
+      while (
+        commentsArr[startingPoint].childrenLevel >= commentsData.childrenLevel
+      ) {
+        startingPoint--;
+      }
+    } catch (error) {
+      startingPoint = undefined;
+    }
+  };
+
+  const removeCommentsCard = (startingPoint, isDelete = false) => {
     if (commentsArr[startingPoint]) {
       while (
         commentsArr[startingPoint].childrenLevel >
@@ -43,7 +61,35 @@ const CommentsCard = ({ index, leftVal, commentsData }) => {
         }
       }
     }
-    setBlog({ ...blog, comments: { results: commentsArr } });
+
+    if (isDelete) {
+      let parentIndex = getParentIndex();
+
+      if (parentIndex !== undefined) {
+        commentsArr[parentIndex].children = commentsArr[
+          parentIndex
+        ].children.filter((child) => child !== _id);
+
+        if (!commentsArr[parentIndex].children.length) {
+          commentsArr[parentIndex].isReplyLoaded = false;
+        }
+      }
+      commentsArr.splice(index, 1);
+    }
+    if (commentsData.childrenLevel === 0 && isDelete) {
+      setTotalParentCommentsLoaded((prev) => prev - 1);
+    }
+    setBlog({
+      ...blog,
+      comments: { results: commentsArr },
+      activity: {
+        ...activity,
+        total_parent_comments:
+          total_parent_comments - commentsData.childrenLevel === 0 && isDelete
+            ? 1
+            : 0,
+      },
+    });
   };
 
   const hideReplies = () => {
@@ -80,6 +126,30 @@ const CommentsCard = ({ index, leftVal, commentsData }) => {
     }
     setReplying((prev) => !prev);
   };
+
+  const deleteComment = (e) => {
+    e.target.setAttribute("disabled", true);
+
+    axios
+      .post(
+        `$  {import.meta.env.VITE_SERVER_LOCATION}/delete-comment`,
+        {
+          _id,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+      )
+      .then(() => {
+        e.target.removeAttribute("disabled");
+        removeCommentsCard(index + 1, true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <>
       <div className="w-full" style={{ paddingLeft: `${leftVal * 10}px` }}>
@@ -88,10 +158,10 @@ const CommentsCard = ({ index, leftVal, commentsData }) => {
             <img
               src={profile_img}
               className="w-6 h-6 rounded-full"
-              alt={`${username} profile image`}
+              alt={`${commented_by_user} profile image`}
             />
             <p className="line-clamp-1">
-              {fullname} @{username}
+              {fullname} @{commented_by_user}
             </p>
             <p className="min-w-fit">{getDay(commentedAt)}</p>
           </div>
@@ -106,18 +176,31 @@ const CommentsCard = ({ index, leftVal, commentsData }) => {
                 <i className="fi fi-rs-comment-dots"></i> Hide Reply
               </button>
             ) : (
-              <button
-                className="text-dark-grey p-2 px-3 hover:bg-grey/30 rounded-md flex items-center gap-2"
-                onClick={loadReplies}
-              >
-                <i className="fi fi-rs-comment-dots"></i> {children.length}
-                Reply
-              </button>
+              <>
+                <button
+                  className="text-dark-grey p-2 px-3 hover:bg-grey/30 rounded-md flex items-center gap-2"
+                  onClick={loadReplies}
+                >
+                  <i className="fi fi-rs-comment-dots"></i> {children.length}
+                  Reply
+                </button>
+              </>
             )}
 
             <button className="underline" onClick={handleReplyClick}>
               Reply
             </button>
+
+            {username == commented_by_user || username == blog_author ? (
+              <button
+                className="p-2 px-3 rounded-md border border-grey ml-auto hover:bg-red/30 flex items-center"
+                onClick={deleteComment}
+              >
+                <i className="fi fi-rr-trash pointer-events-none"></i>
+              </button>
+            ) : (
+              ""
+            )}
           </div>
           {isReplying ? (
             <div className="mt-8">
--- server/index.js
+++ server/index.js
@@ -608,3 +608,60 @@ app.post("/get-replies", (req, res) => {
       return res.status(500).json({ error: err.message });
     });
 });
+
+const deleteComments = (_id) => {
+  Comment.findOneAndDelete({ _id })
+    .then((comment) => {
+      if (comment.parent) {
+        Comment.findOneAndUpdate(
+          { _id: comment.parent },
+          { $pull: { children: _id } }
+        )
+          .then((data) => {
+            console.log("comment delete from parent");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+      Notification.findOneAndDelete({ comment: _id }).then((notification) =>
+        console.log("comment notification deleted")
+      );
+
+      Notification.findOneAndDelete({ reply: _id }).then((notification) =>
+        console.log("reply notification deleted")
+      );
+
+      Blog.findOneAndUpdate(
+        { _id: comment.blog_id },
+        {
+          $pull: { comments: _id },
+          $inc: { "activity.total_comments": -1 },
+          "activity.total_parent_comments": comment.parent ? 0 : -1,
+        }
+      ).then((blog) => {
+        if (comment.children_length) {
+          comment.children.map((replies) => {
+            deleteComments(replies);
+          });
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+app.post("/delete-comment", verifyJWT, (req, res) => {
+  const user_id = req.user;
+
+  const { _id } = req.body;
+
+  Comment.findOne({ _id }).then((comment) => {
+    if (user_id == comment.commented_by || user_id == comment.blog_author) {
+      deleteComments(_id);
+      return res.status(200).json({ status: "done" });
+    } else {
+      return res.status(403).json({ error: "You cannot delete this comment" });
+    }
+  });
+});
