528188b84b4921ae8c97c669ffcfaaea3a8020b7
Danish <dan71ish@gmail.com>
2024-04-10 20:09:58 +0530

    manage blog

--- client/src/pages/manage-blogs.page.jsx
+++ client/src/pages/manage-blogs.page.jsx
@@ -1,8 +1,10 @@
 import { useContext, useState } from "react";
 import Loader from "../components/loader.component";
+import { Toaster, toast } from "react-hot-toast";
 import axios from "axios";
 import { UserContext } from "../App";
 import { filterPagination } from "../common/filter-pagination-data";
+import { useEffect } from "react";
 
 const ManageBlog = () => {
   const [blogs, setBlogs] = useState(null);
@@ -49,9 +51,42 @@ const ManageBlog = () => {
       });
   };
 
+  useEffect(() => {
+    if (access_token) {
+      if (blogs === null) {
+        getBlogs({ page: 1, draft: false });
+      }
+      if (drafts === null) {
+        getBlogs({ page: 1, draft: true });
+      }
+    }
+  }, [access_token, blogs, drafts, query]);
+
+  let intervalId;
+  const handleSearch = (e) => {
+    let search = e.target.value;
+    if()
+    clearInterval(intervalId);
+    setTimeout(() => {
+      setQuery(search);
+    }, 800);
+  };
+  const handleChange = () => {};
+
   return (
     <>
-      <div>ManageBlog</div>
+      <h1 className="max-md:hidden">Manage Blogs</h1>
+      <Toaster />
+      <div className="relative max-md:mt-5 md:mt-8 mb-10">
+        <input
+          type="search"
+          className="w-full bg-grey p-4 pl-12 pr-6 rounded-full placeholder:text-dark-grey"
+          placeholder="Search Blogs"
+          onChange={handleChange}
+          onKeyDown={handleSearch}
+        />
+        <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
+      </div>
     </>
   );
 };
